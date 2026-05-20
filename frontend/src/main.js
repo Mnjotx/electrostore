2c347793e18b15b3eb42d057e7ae4f7a45c55dd2
Dishant1-git <Dishantkaushal456@gmail.com>
2026-05-20 16:46:56 +0530

    update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -829,7 +829,7 @@ export const Main = () => {
                     <div className="marquee py-5">
                         <div className="marquee-content gap-5">
                             {br.concat(br).map((a, index) => (
-                                <img key={index} className='rounded-4 object-fit-cover' src={`${a.Img}`} height="100px" alt="brand" />
+                                <img key={index} className='rounded-4 object-fit-cover' src={`/uploads/${a.Img}`} height="100px" alt="brand" />
                             ))}
                         </div>
                     </div>
